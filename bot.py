6b237f012d405237a9261273e338b55ebc520b56
vladimir <pylin.vs@gmail.com>
2023-08-27 19:28:23 +0500

    First version of the bot. Added buttons to the first question.

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
-from aiogram import Bot, Dispatcher
+from typing import Any
 import yaml
+from aiogram import Bot, Dispatcher, F
+from aiogram.dispatcher import router
 from aiogram.filters import Command
+from aiogram.handlers import InlineQueryHandler
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.types import Message
+    InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResult, InlineQueryResultsButton, InlineQueryResultArticle, \
+    InputTextMessageContent
+from aiogram.types import Message, InlineQuery
+
+TEXT_BUTTON_1 = "Забронировать место на парковке"
+TEXT_BUTTON_2 = "Вывести список брони"
+START_MESSAGE = "Привет!\nМеня зовут Анна.\nПомогу забронировать место на парковке."
+HELP_MESSAGE = "Напиши мне что-нибудь"
 
 # Получаем данные из файла настроек
 with open('settings.yml', 'r') as file:
@@ -14,25 +23,38 @@ API_TOKEN = CONSTANTS['API_TOKEN']
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
+button_1: KeyboardButton = KeyboardButton(text=TEXT_BUTTON_1)
+button_2: KeyboardButton = KeyboardButton(text=TEXT_BUTTON_2)
+
+# Создаем объект клавиатуры, добавляя в него кнопки
+keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+    keyboard=[[button_1, button_2]],
+    resize_keyboard=True
+)
+
 
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    await message.answer(
+        START_MESSAGE,
+        reply_markup=keyboard
+    )
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ '
-                         'я пришлю тебе твое сообщение')
+    await message.answer(HELP_MESSAGE)
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
-@dp.message()
-async def send_echo(message: Message):
-    await message.reply(text=message.text)
+# Этот хэндлер будет срабатывать на просьбу забронировать место и удалять клавиатуру
+@dp.message(F.text == TEXT_BUTTON_1)
+async def process_dog_answer(message: Message):
+    await message.reply(
+        text='Сейчас посмотрим, что я могу Вам предложить...',
+        reply_markup=ReplyKeyboardRemove()
+    )
 
 
 if __name__ == '__main__':
